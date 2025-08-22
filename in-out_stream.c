1332bc903670bf3d6034f728bea9249f5fd60a9f
panth37r <pantherbtw@bk.ru>
2025-08-22 21:52:29 +0300

    in-out functions : load-data/file, memdump

---
+++ in-out_stream.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "pdp.h"
+
+
+void load_data(FILE *stream)
+{
+	Address adr;
+	word count;
+	byte data;
+	while(fscanf(stream, "%hx %hx", &adr, &count) == 2)
+	{
+		for(int i = 0; i < count; i++)
+		{
+			fscanf(stream, "%hhx", &data);
+			b_write(adr+i, data);
+		}
+	}
+}
+
+void mem_dump(FILE *stream, Address adr, int size)
+{
+	for(int i = 0; i < size; i += 2)
+	{
+		fprintf(stream, "%06o: %06o %04x\n", adr+i, w_read(adr+i), w_read(adr+i));
+	}
+}
+
+
+
+void load_file(const char* filename)
+{
+	FILE *stream = fopen(filename, "r");
+	if(stream == NULL)
+		perror("Error: can't open file");
+	load_data(stream);
+	fclose(stream);
+}
